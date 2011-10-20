33b636399ec4831d56e5a182780f4cafe283e871
Wayne Graham <wayne.graham@virginia.edu>
2011-10-20 09:42:42 -0400

    Set default controller to pages#index

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ Testing::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'pages#index'
 
   # See how all your routes lay out with "rake routes"
 
