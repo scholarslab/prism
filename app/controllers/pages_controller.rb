a1b91dd1790b0126b3324e94dba6e3812414c415
Wayne Graham <wayne.graham@virginia.edu>
2011-10-20 10:11:55 -0400

    Tests for pages view; added sqlite3 files to ignore list

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def index
+    @title = "Home"
   end
 
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,11 +2,12 @@ require 'spec_helper'
 
 describe PagesController do
 
+  render_views
+
   describe "GET 'index'" do
     it "returns http success" do
       get 'index'
       response.should be_success
     end
   end
-
 end
