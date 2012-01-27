c0715bf63cc61889b94d3f313c9d218e3ed729c1
Alex Gil <colibri.alex@gmail.com>
2012-01-27 14:25:24 -0500

    tests failing right now

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -6,8 +6,25 @@ describe PagesController do
 
   describe "GET 'index'" do
     it "returns http success" do
-      get 'index'
+      get :index
       response.should be_success
+    end
+
+    describe "when user is not signed in" do
+      it "should have a sign in link" do
+        get 'index'
+        response.should have_selector('a', :content => 'sign in')
+      end
+      it "should have a sign up link" do
+        get 'index'
+        response.should have_selector('a', :content => 'Sign up')
+      end
+    end
+
+    describe "when user is signed in" do
+      it "should have a sign out link" do
+        pending
+      end 
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,19 +4,3 @@ describe User do
 
 end
 
-describe "GET index" do
-  describe "when user is not signed in" do
-    it "should have a sign in link" do
-      response.should have_selector('a', :content => 'sign in')
-    end
-    it "should have a sign up link" do
-      response.should have_selector('a', :content => 'Sign up')
-    end
-  end
-
-  describe "when user is signed in" do
-    it "should have a sign out link" do
-      pending
-    end 
-  end
-end
