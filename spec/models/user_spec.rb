072749eb8b5e2ebe5f23926b97acf378f89af628
Alex Gil <colibri.alex@gmail.com>
2012-01-27 14:08:25 -0500

    wrote tests for the user div in the index pages

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+end
+
+describe "GET index" do
+  describe "when user is not signed in" do
+    it "should have a sign in link" do
+      response.should have_selector('a', :content => 'sign in')
+    end
+    it "should have a sign up link" do
+      response.should have_selector('a', :content => 'Sign up')
+    end
+  end
+
+  describe "when user is signed in" do
+    it "should have a sign out link" do
+      pending
+    end 
+  end
 end
