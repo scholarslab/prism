5c9dffb6ee2422fe7a22e4f378425a5fc9109172
Joanna Swafford <jes8zv@virginia.edu>
2012-02-07 11:49:38 -0500

    Prism_model and associations for other models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :markings
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120127181536) do
+ActiveRecord::Schema.define(:version => 20120207163308) do
 
   create_table "documents", :force => true do |t|
     t.string   "title"
@@ -20,11 +20,17 @@ ActiveRecord::Schema.define(:version => 20120127181536) do
     t.text     "description"
     t.string   "format"
     t.text     "content"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   add_index "documents", ["title"], :name => "index_documents_on_title"
+
+  create_table "prisms", :force => true do |t|
+    t.text     "prompt"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
