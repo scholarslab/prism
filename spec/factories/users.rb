45252c03291fd963607dd3218af3daf5ea6d9031
Wayne Graham <wayne.graham@virginia.edu>
2012-03-06 11:22:22 -0500

    Updates the FactoryGirl definition for users to remove deprecated version of Factory generator

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 # Read about factories at http://github.com/thoughtbot/factory_girl
 
-Factory.define :user do |user|
+FactoryGirl.define :user do |user|
   user.email "fred.foonly@example.com"
   user.password "my_password"
 end
