73be2da2a4aa6db7fef4019046abfd5cf4e4973a
Wayne Graham <wayne.graham@virginia.edu>
2012-03-14 09:06:57 -0400

    Add model annotation on models and specs

--- spec/models/facet_spec.rb
+++ spec/models/facet_spec.rb
@@ -3,3 +3,14 @@ require 'spec_helper'
 describe Facet do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+# == Schema Information
+#
+# Table name: facets
+#
+#  id         :integer         not null, primary key
+#  color      :string(255)
+#  category   :string(255)
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
