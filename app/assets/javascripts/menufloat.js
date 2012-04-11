96e93f5028d9976d4979f6acc3bb8dfd7d48316a
Eric Rochester <err8n@virginia.edu>
2012-04-11 10:05:18 -0400

    Use local scoping for variables and check for #tools being on the page.

--- app/assets/javascripts/menufloat.js
+++ app/assets/javascripts/menufloat.js
@@ -1,24 +1,29 @@
 function positionMenu() {
 
-    windowScrollTop = $(window).scrollTop();
-    menuTop = $('#tools').offset().top;
-    windowHeight = $(window).height();
-    menuHeight = $('#tools').height();
-    footerHeight = $('footer').height();
-    visibleHeight = menuHeight + footerHeight + 50;
+    // This gets called on every page, whether the tools are available or not.
+    // This checks for that.
+    $('#tools').each(function() {
+        var tools = $(this);
+        var windowScrollTop = $(window).scrollTop();
+        var menuTop = tools.offset().top;
+        var windowHeight = $(window).height();
+        var menuHeight = tools.height();
+        var footerHeight = $('footer').height();
+        var visibleHeight = menuHeight + footerHeight + 50;
 
-    // console.log('Visible Height:'+ visibleHeight, 'Window:'+ windowHeight,
-    //             '1st cond: ' + (windowScrollTop > menuTop),
-    //             '2nd cond: ' + (windowHeight > visibleHeight));
+        // console.log('Visible Height:'+ visibleHeight, 'Window:'+ windowHeight,
+        //             '1st cond: ' + (windowScrollTop > menuTop),
+        //             '2nd cond: ' + (windowHeight > visibleHeight));
 
-    // If the scroll top of the window is greater than the top of the
-    // menu, add class fixed.
+        // If the scroll top of the window is greater than the top of the
+        // menu, add class fixed.
 
-    if (windowScrollTop > menuTop && windowHeight > visibleHeight)  {
-        $('#sidebar').addClass('fixed');
-    } else {
-        $('#sidebar').removeClass('fixed');
-    }
+        if (windowScrollTop > menuTop && windowHeight > visibleHeight)  {
+            $('#sidebar').addClass('fixed');
+        } else {
+            $('#sidebar').removeClass('fixed');
+        }
+    });
 }
 $(window).resize(positionMenu);
 $(window).scroll(positionMenu);
