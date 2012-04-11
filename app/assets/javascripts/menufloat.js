6930cc4f9276e4fa382a12ee56119abde1d34cf7
Eric Rochester <err8n@virginia.edu>
2012-04-11 10:07:34 -0400

    Lining things up.

--- app/assets/javascripts/menufloat.js
+++ app/assets/javascripts/menufloat.js
@@ -3,13 +3,13 @@ function positionMenu() {
     // This gets called on every page, whether the tools are available or not.
     // This checks for that.
     $('#tools').each(function() {
-        var tools = $(this);
+        var tools           = $(this);
         var windowScrollTop = $(window).scrollTop();
-        var menuTop = tools.offset().top;
-        var windowHeight = $(window).height();
-        var menuHeight = tools.height();
-        var footerHeight = $('footer').height();
-        var visibleHeight = menuHeight + footerHeight + 50;
+        var menuTop         = tools.offset().top;
+        var windowHeight    = $(window).height();
+        var menuHeight      = tools.height();
+        var footerHeight    = $('footer').height();
+        var visibleHeight   = menuHeight + footerHeight + 50;
 
         // console.log('Visible Height:'+ visibleHeight, 'Window:'+ windowHeight,
         //             '1st cond: ' + (windowScrollTop > menuTop),
@@ -24,6 +24,7 @@ function positionMenu() {
             $('#sidebar').removeClass('fixed');
         }
     });
+
 }
 $(window).resize(positionMenu);
 $(window).scroll(positionMenu);
