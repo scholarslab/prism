8461c4dbdab87eb51908deb7600a2bde0389c73c
Joanna Swafford <jes8zv@virginia.edu>
2012-04-30 22:34:04 -0400

    fixed password confirmation field

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :token_authenticatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me
 end
 # == Schema Information
 #
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120403193447) do
+ActiveRecord::Schema.define(:version => 20120501022605) do
 
   create_table "documents", :force => true do |t|
     t.string   "title"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(:version => 20120403193447) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "authentication_token"
+    t.string   "password_confirmation"
   end
 
   add_index "users", ["authentication_token"], :name => "index_users_on_authentication_token", :unique => true
