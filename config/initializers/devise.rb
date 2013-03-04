d560e255e96d1c82b6e3d10de088899f2e0e292d
Brandon Walsh Test 2 <bmw9t2@d-172-25-72-69.bootp.virginia.edu>
2013-03-04 14:12:01 -0500

    Refs #109. Deleted an unncessary/unsuccessful workaround for the Google authentication. All three are working - the errors I got were local to my computer.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -214,10 +214,9 @@ require "omniauth-facebook"
 require "omniauth-twitter"
 require "omniauth-google"
 
-  config.omniauth :facebook, "433312300089516", "6e5d8dc6f680a4d9b9b40004166f659a"
+  config.omniauth :facebook, "433312300089516", "6e5d8dc6f680a4d9b9b40004166f659a", {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
   config.omniauth :twitter, 'muekOMJXBKXRWEUnVGG0pA', 'PtUsO6guIPDPn9x4OUQQGvU6yGqD8fswEiAoECBOq4'
-  config.omniauth :google, "759053607189.apps.googleusercontent.com", "1Z8PJatEWV-q0oJ3jgm3IvSr",
-     :client_options => {:ssl => {:ca_path => '/Users/Brandon/.rvm/usr/ssl'}}
+  config.omniauth :google, "759053607189.apps.googleusercontent.com", "1Z8PJatEWV-q0oJ3jgm3IvSr"
 
 
 
