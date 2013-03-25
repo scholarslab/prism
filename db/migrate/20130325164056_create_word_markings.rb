8b7fe95703f379d2d989788a1f3e931f2219d8fd
Shane Lin <shanelin@AllegrettoLinux.(none)>
2013-03-25 16:51:36 -0400

    Forgot to add stuff

--- db/migrate/20130325164056_create_word_markings.rb
+++ db/migrate/20130325164056_create_word_markings.rb
@@ -2,10 +2,10 @@ class CreateWordMarkings < ActiveRecord::Migration
   def change
     create_table :word_markings do |t|
       t.integer :index
-      t.integer :facet1_count
-      t.integer :facet2_count
-      t.integer :facet3_count
-      t.integer :facet4_count
+      t.integer :facet1_count, :default => 0
+      t.integer :facet2_count, :default => 0
+      t.integer :facet3_count, :default => 0
+      t.integer :facet4_count, :default => 0
       t.references :prism
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120501022605) do
+ActiveRecord::Schema.define(:version => 20130325181908) do
 
   create_table "documents", :force => true do |t|
     t.string   "title"
@@ -50,6 +50,16 @@ ActiveRecord::Schema.define(:version => 20120501022605) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.integer  "document_id"
+    t.string   "title"
+    t.string   "author"
+    t.text     "content"
+    t.boolean  "sandbox"
+    t.integer  "num_words"
+    t.string   "facet1"
+    t.string   "facet2"
+    t.string   "facet3"
+    t.string   "facet4"
+    t.string   "description"
   end
 
   create_table "users", :force => true do |t|
@@ -72,5 +82,18 @@ ActiveRecord::Schema.define(:version => 20120501022605) do
   add_index "users", ["authentication_token"], :name => "index_users_on_authentication_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+
+  create_table "word_markings", :force => true do |t|
+    t.integer  "index"
+    t.integer  "facet1_count", :default => 0
+    t.integer  "facet2_count", :default => 0
+    t.integer  "facet3_count", :default => 0
+    t.integer  "facet4_count", :default => 0
+    t.integer  "prism_id"
+    t.datetime "created_at",                  :null => false
+    t.datetime "updated_at",                  :null => false
+  end
+
+  add_index "word_markings", ["prism_id"], :name => "index_word_markings_on_prism_id"
 
 end
