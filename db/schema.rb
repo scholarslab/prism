8195aabc79c936a5deda1a7b430792a20dc2b0c8
Eric Rochester <erochest@virginia.edu>
2013-04-04 09:10:50 -0400

    Updated the DB schema.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130308024542) do
+ActiveRecord::Schema.define(:version => 20130329185236) do
 
   create_table "documents", :force => true do |t|
     t.string   "title"
@@ -69,7 +69,7 @@ ActiveRecord::Schema.define(:version => 20130308024542) do
     t.string   "password_confirmation"
     t.string   "provider"
     t.string   "uid"
-    t.string   "name"
+    t.integer  "facet_num"
   end
 
   add_index "users", ["authentication_token"], :name => "index_users_on_authentication_token", :unique => true
