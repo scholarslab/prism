9e1649765723cd19e86b0782da85f6ff0632819e
Brandon Walsh <bmw9t2@d-172-25-73-11.bootp.virginia.edu>
2013-04-08 15:12:55 -0400

    Adds language attribute to the Prism model.

--- db/migrate/20130408184920_add_meta_datato_prism_model.rb
+++ db/migrate/20130408184920_add_meta_datato_prism_model.rb
@@ -3,5 +3,6 @@ class AddMetaDatatoPrismModel < ActiveRecord::Migration
   	add_column :prisms, :user_id, :integer
   	add_column :prisms, :unlisted, :boolean
   	add_column :prisms, :publication_date, :integer
+  	add_column :prisms, :language, :string
   end
 end
