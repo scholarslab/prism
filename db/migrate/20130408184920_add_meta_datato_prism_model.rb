8d08abfff9d75c67220e849371486a63148c2df4
Brandon Walsh <bmw9t2@d-172-25-73-137.bootp.virginia.edu>
2013-04-11 11:36:08 -0400

    Fixes the views for the user upload page to show the appropriate metapage.

--- db/migrate/20130408184920_add_meta_datato_prism_model.rb
+++ db/migrate/20130408184920_add_meta_datato_prism_model.rb
@@ -2,8 +2,6 @@ class AddMetaDatatoPrismModel < ActiveRecord::Migration
   def change
   	add_column :prisms, :user_id, :integer
   	add_column :prisms, :unlisted, :boolean
-  	add_column :prisms, :publication_date, :integer
-  	add_column :prisms, :language, :string
-  	remove_column :prisms, :prompt
+  	add_column :prisms, :publication_date, :string
   end
 end
