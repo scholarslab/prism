84e5d67dfbcd55e5074b6473c792d7dbf181e8a0
Wayne Graham <wayne.graham@virginia.edu>
2013-04-18 11:13:33 -0400

    Fixes tests
    Updates tests to use 'Prism' object rather than the 'Document' object
    Adds factories for a default Prism and one that is unlisted
    Moves view specs for documents to prisms
    Removes marking spec

--- spec/factories/prisms.rb
+++ spec/factories/prisms.rb
@@ -1,7 +1,24 @@
 # Read about factories at http://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
+
   factory :prism do
-    prompt "MyText"
+    title Forgery::LoremIpsum.words(1)
+    author Forgery::Name.full_name
+    content Forgery::LoremIpsum.paragraphs(5)
+    description Forgery::LoremIpsum.paragraphs(1)
+    publication_date Forgery::Basic.number
+    license 'cc0'
   end
+
+  factory :prism_unlisted do
+    title Forgery::LoremIpsum.words(1)
+    author Forgery::Name.full_name
+    content Forgery::LoremIpsum.paragraphs(5)
+    description Forgery::LoremIpsum.paragraphs(1)
+    publication_date Forgery::Basic.number
+    unlisted true
+    license 'by'
+  end
+
 end
