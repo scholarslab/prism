bfae5eca226dfc8cbbee86f6acbfe63ec0c8c07f
Brandon Walsh <bmw9t@virginia.edu>
2013-04-22 15:13:03 -0400

    Pushes some broken stuff to fix the data on the myprisms page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])	
-    @prisms = @user.prisms
+	    @prisms = @user.prisms
+		
+		# @word_markings = @user.word_markings
+		@highlighted_prisms = @user.word_markings.prism.uniq
+		
 	end
 end
