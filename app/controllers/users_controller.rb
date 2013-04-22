fc29c107e91c0e6af1227513fc8ba4d04ee8941f
Wayne Graham <wayne.graham@virginia.edu>
2013-04-22 16:04:19 -0400

    Fixes the my prisms page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,9 @@
 class UsersController < ApplicationController
-	def show
-		@user = User.find(params[:id])	
-	    @prisms = @user.prisms
-		
-		# @word_markings = @user.word_markings
-		@highlighted_prisms = @user.word_markings.prism.uniq
-		
-	end
+  def show
+    @user = User.find(params[:id])	
+    @prisms = @user.prisms
+
+    @highlighted_prisms = @user.word_markings.map(&:prism_id).uniq
+
+  end
 end
