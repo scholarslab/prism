31cb0d13c4e90add341f208d80af3261e37548cc
Brandon Walsh <bmw9t@virginia.edu>
2013-04-30 12:57:11 -0400

    Tries hard coding the Facebook redirect url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :set_locale
+  before_filter :check_url
+
+def check_url
+  url = request.url
+  if url.include?('facebook')
+    redirect_to ('http://www.prism-staging12.herokuapp.com/users/auth/facebook')        
+  end
+end
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
