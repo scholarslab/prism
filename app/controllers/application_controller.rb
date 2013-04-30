27fa6d8650be509dd00c10f65e16e96e9310b10a
Brandon Walsh <bmw9t@virginia.edu>
2013-04-30 13:35:30 -0400

    Fixes last commit.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :set_locale
-  before_filter :check_url
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
