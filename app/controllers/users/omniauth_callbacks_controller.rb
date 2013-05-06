58b97c41c30e0571d2ad2505c0477ce44bc0d177
Brandon Walsh <bmw9t@virginia.edu>
2013-05-06 11:11:28 -0400

    further fixes the new google oauth2 function.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -34,7 +34,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
       sign_in_and_redirect @user, :event => :authentication
     else
-      session["devise.google_data"] = request.env["omniauth.auth"]
+      session["devise.google_oauth2_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
   end
