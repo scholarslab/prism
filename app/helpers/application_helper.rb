e2bf776913af28d37d8eb7f85d1e9bae11e758a8
Brandon Walsh <bmw9t@virginia.edu>
2013-05-07 12:04:50 -0400

    Tries to fix the url link for the default image.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,7 +20,7 @@ module ApplicationHelper
 
   # Use this with <%= image_tag avatar_url(user) %>.
   def avatar_url(user)
-    default_url = "#{root_url}images/guest.jpg"
+    default_url = "#{request.env["HTTP_HOST"]}/#{image_path('guest.jpg')}"
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     url = "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
   end
