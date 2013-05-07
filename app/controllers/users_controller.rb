984c46a4e19c845670e88771142337bba0ec0d2d
Brandon Walsh <bmw9t@virginia.edu>
2013-05-07 10:55:59 -0400

    Closes #271. It is working, but wayne wants to make it better so I will not close.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
+
   def show
     @user = current_user	
-    @prisms = @user.prisms
-
-    @highlighted_prisms = @user.word_markings.map(&:prism_id).uniq
-
-    @title = "myprisms"
+    if @user != nil
+    	@prisms = @user.prisms
+    	@highlighted_prisms = @user.word_markings.map(&:prism_id).uniq
+	    @title = "myprisms"
+    else
+    	redirect_to :home
+    	flash[:error] ="Sign in first!"
+    end
   end
 end
