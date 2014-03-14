e63a194ee1086ce086fe7b547d41daa70123a70c
Brandon Walsh <bmw9t@virginia.edu>
2014-03-13 20:20:04 -0400

    Adds commenting on the models for prisms, facets, word_markings.

--- app/models/facet.rb
+++ app/models/facet.rb
@@ -2,5 +2,9 @@ class Facet < ActiveRecord::Base
   has_many :word_markings
   belongs_to :prism
  
+# Facets are the parameters along which a user will mark a particular prism. 
+# In other words, they are the pre-selected categories along which a reader will be interpreting the document in question.
+# Facets are selected by the uploader of a particular prism.
+
   attr_accessible :description, :prism_id, :order, :color
 end
--- app/models/prism.rb
+++ app/models/prism.rb
@@ -5,6 +5,10 @@ class Prism < ActiveRecord::Base
   attr_accessible :title, :author, :content, :num_words, :description, :user_id, :unlisted, :publication_date, :language, :license, :uuid
   validates_presence_of :title, :content, :license
 
+# A prism is the document a user wishes other readers to interpret along particular facets.
+# In other words, a prism is an uploaded text which a group of readers is expected to mark along pre-selected interpretive categories.
+# A document, when uploaded, is split into an array consisting of individual words.
+
   self.primary_key ='uuid'
 
   before_create :set_uuid
