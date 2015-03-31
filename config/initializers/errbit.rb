1bcf2377d5d2789f4260577cd12fa54822f741e3
Wayne Graham <wayne.graham@virginia.edu>
2015-03-31 11:25:17 -0400

    Remove errbit depency

--- config/initializers/errbit.rb
+++
@@ -1,6 +0,0 @@
-Airbrake.configure do |config|
-  config.api_key = '1f17e220bf987606b12277e9d9ec76ae'
-  config.host    = 'slab-errbit.herokuapp.com'
-  config.port    = 80
-  config.secure  = config.port == 443
-end
