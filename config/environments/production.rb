91c4c5ec6d572fac9bc53c803679d5a514389ad6
Wayne Graham <wayne.graham@virginia.edu>
2015-10-21 09:57:27 -0400

    Handle deprecation notices in the log in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,6 +39,8 @@ Testing::Application.configure do
   # Use a different cache store in production
   config.cache_store = :dalli_store, 'localhost', { :namespace => "prism", :expires_in => 1.day, :compress => true }
 
+  config.active_support.deprecation = :log # how to handle deprecation notices
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
