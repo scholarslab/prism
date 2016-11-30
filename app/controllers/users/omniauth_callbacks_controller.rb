ce27e29d2f8dc332160858ab3f10d95baa588cb1
Brandon Walsh <bmw9t@virginia.edu>
2016-11-30 08:49:12 -0500

    closes #332 by removing the login options for Persona. Persona is shutting down, so the logins will fail in the future.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -25,17 +25,4 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
   end
 
-  def browser_id
-    @user = User.find_for_browserid_oauth(
-      request.env['omniauth.auth'], current_user
-    )
-
-    if @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Mozilla Persona"
-      sign_in_and_redirect @user, :event => :authentication
-    else
-      session['devise.browserid_data'] = request.env['omniauth.auth']
-      redirect_to new_user_registration_url
-    end
-  end
 end
